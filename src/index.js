b08cf20d97943fd40141face09ff3b8c2d345885
saraliva4 <122287923+saraliva4@users.noreply.github.com>
2023-03-12 14:49:02 +0100

    Fixing layout in HTML/CSS and fixing search-bar in JS
    Fixing some bugs, and changing layout of search bar.
    Fixing JavaScript to make search bar work.

--- src/index.js
+++ src/index.js
@@ -28,28 +28,48 @@ todayDate.innerHTML = `${weekDays[now.getDay()]}, ${
   months[now.getMonth()]
 } ${now.getDate()}`;
 
-//LocationTemperature
 function showTemperature(response) {
   let temperature = Math.round(response.data.main.temp);
+  let temperatureElement = document.querySelector(".current-temperature");
+  temperatureElement.innerHTML = `${temperature}`;
   let city = response.data.name;
-  let currentDegrees = document.querySelector("#current-degrees");
-  currentDegrees.innerHTML = `${temperature}`;
-  let currentLocation = document.querySelector("#current-location");
-  currentLocation.innerHTML = `${city}`;
+  let cityElement = document.querySelector("#current-location");
+  cityElement.innerHTML = `${city}`;
+  let description = response.data.weather[0].description;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = `${description}`;
+  let humidity = response.data.main.humidity;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = `${humidity}`;
+  let windSpeed = response.data.wind.speed;
+  let windSpeedElement = document.querySelector("#wind-speed");
+  windSpeedElement.innerHTML = Math.round(windSpeed);
+  let iconElement = document.querySelector("#weather-icon");
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", `response.data.weather[0].description`);
 }
 
-function searchLocation(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#search-input");
+function searchLocation(city) {
   let apiKey = "7746bdeabca928cfedcad71e52fd9d66";
   let unit = `metric`;
-  let city = `${searchInput.value}`;
-  let apiUrlCity = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
+  let cityInput = city;
+  let apiUrlCity = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}&units=${unit}`;
   axios.get(`${apiUrlCity}`).then(showTemperature);
 }
 
+function handleSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+  searchLocation(searchInput.value);
+}
+
+searchLocation("longyearbyen");
+
 let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", searchLocation);
+searchForm.addEventListener("submit", handleSubmit);
 
 function showPositionFromButton(position) {
   let latitude = position.coords.latitude;
@@ -67,3 +87,21 @@ function getCurrentLocation(event) {
 
 let currentLocationButton = document.querySelector("#current-location-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
+
+//****
+
+let temperatureElement = document.querySelector(".current-temperature");
+
+function temperatureCelsius(event) {
+  event.preventDefault();
+  currentDegrees.innerHTML = 3;
+}
+let celsius = document.querySelector("#celsius");
+celsius.addEventListener("click", temperatureCelsius);
+
+function temperatureFahrenheit(event) {
+  event.preventDefault();
+  currentDegrees.innerHTML = 37;
+}
+let fahrenheit = document.querySelector("#fahrenheit");
+fahrenheit.addEventListener("click", temperatureFahrenheit);
