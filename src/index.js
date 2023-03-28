00d9b1cf862de47e5733512e529a77862709258e
saraliva4 <122287923+saraliva4@users.noreply.github.com>
2023-03-28 15:02:40 +0200

    Replacing forecast data with data from API

--- src/index.js
+++ src/index.js
@@ -3,27 +3,39 @@ function getForecast(coordinates) {
   let unit = "metric";
   let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=${unit}`;
   axios.get(apiURL).then(showWeeklyForecast);
+  axios.get(apiURL).then(showHourlyForecast);
 }
-function showHourlyForecast() {
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+
+  return weekDays[day];
+}
+
+function showHourlyForecast(response) {
   let hourlyForecastElement = document.querySelector("#hourly-forecast");
   let hourlyForecastHTML = `<div class="row">`;
 
-  weekDays.forEach(function (day) {
-    hourlyForecastHTML =
-      hourlyForecastHTML +
-      `
+  weekDays.forEach(function (forecastHour, index) {
+    if (index < 6) {
+      hourlyForecastHTML =
+        hourlyForecastHTML +
+        `
     <div class="col-2 today-hours currently">
     ${day}
     <br />
-    <img
-    src=""
-    alt=""
-    width="42"
-    class="forecast-weather-icon"
-    id="forecast-weather-icon"
-    />
+    <div class="col-6 next-week weather">
+              <img class="next-week weather-emoji"
+              src="https://openweathermap.org/img/wn/${forecastHour.weather[0].icon}@2x.png"
+              alt=""
+              width="42"
+              class="forecast-weather-icon"
+              id="forecast-weather-icon"
+              />
     3°
     </div>`;
+    }
   });
 
   hourlyForecastHTML = hourlyForecastHTML + `</div>`;
@@ -31,31 +43,49 @@ function showHourlyForecast() {
 }
 
 function showWeeklyForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let weeklyForecastElement = document.querySelector("#weekly-forecast");
   let weeklyForecastHTML = `<div class="row">`;
-  weekDays.forEach(function (day) {
-    weeklyForecastHTML =
-      weeklyForecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      weeklyForecastHTML =
+        weeklyForecastHTML +
+        `
       <div class="col-12 next-week-card card" style="width: 30rem">
         <div class="next-week-card card-body">
-          <h5 class="next-week card-title" id="weekday-one">${day}</h5>
+          <h5 class="next-week card-title" id="weekday-one">${formatDay(
+            forecastDay.dt
+          )}</h5>
           <h6 class="next-week card-subtitle mb-2 text-muted" id="date-one">
             January 23rd
           </h6>
           <div class="row">
             <div class="col-6 next-week weather">
-              <span class="next-week weather-emoji">🌧</span>
+              <img class="next-week weather-emoji"
+              src="https://openweathermap.org/img/wn/${
+                forecastDay.weather[0].icon
+              }@2x.png"
+              alt=""
+              width="42"
+              class="forecast-weather-icon"
+              id="forecast-weather-icon"
+              />
               <span class="next-week weather-forecast">Rain</span>
               </div>
-            <div class="col-6 next-week temperature">4°/1°</div>
+            <div class="col-6 next-week temperature">
+            <span class ="max-temp"> <strong> ${Math.round(
+              forecastDay.temp.max
+            )}°</strong> </span> / <span class="min-temp">${Math.round(
+          forecastDay.temp.min
+        )}°</span>
+            </div>  
           </div>
         </div>
-        </div>
-         `;
-    weeklyForecastHTML = weeklyForecastHTML + `</div>`;
-    weeklyForecastElement.innerHTML = weeklyForecastHTML;
+      </div>
+      `;
+      weeklyForecastHTML = weeklyForecastHTML + `</div>`;
+      weeklyForecastElement.innerHTML = weeklyForecastHTML;
+    }
   });
 }
 
